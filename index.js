55bd3c9ce0e29386e834091d3d3e4664bcbfa339
oscar sarabia <osarabia@convene-tech.com>
2025-12-17 10:29:11 -0600

    deleting old files
    deleting old files

--- index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import InitialScreen from './src/core/presentation/common/screens/InitialScreen';
-import {name as appName} from './app.json';
-AppRegistry.registerComponent(appName, () => InitialScreen);
-
--- ios/Pods/Target Support Files/Pods-ArquetipoReactNativeCLIApp/Pods-ArquetipoReactNativeCLIApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ArquetipoReactNativeCLIApp : NSObject
-@end
-@implementation PodsDummy_Pods_ArquetipoReactNativeCLIApp
-@end
